8c1371158fcc2c15d8ef8b886d30b94e79885bdf
kevinlee1989 <123864707+kevinlee1989@users.noreply.github.com>
2024-10-01 21:50:59 +0000

    Edit

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  char hello[] = "My name is Kevin Lee"; 
+  char hello[] = "My name is Kevin Lee!!!!!!!!!!!!"; 
   char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
 
   puts(hello);
